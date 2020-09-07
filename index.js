72c2d5e950234c0592713be1bf5c2f4b214ba683
Notorius Whale <notoriuswhale@outlook.com>
2020-09-07 03:21:53 +0300

    added react frontend

--- index.js
+++ index.js
@@ -1,93 +1,100 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
 
-const PORT = 5000;
-const router = require('./router');
+const PORT = process.env.PORT || 80;
+const router = require("./router");
 
-const {addUser, removeUser, getUser, getUsersInRoom, startGame, getGameState, movePiece, updateBoardState} = require('./users');
+const {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom,
+    startGame,
+    getGameState,
+    movePiece,
+    updateBoardState,
+} = require("./users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
-    console.log('We have a new connection');
+io.on("connection", (socket) => {
+    console.log("We have a new connection");
 
-    socket.on('join', ({room, random}, callback) => {
-
-        const {error, user} = addUser({id: socket.id, room: room, random});
+    socket.on("join", ({ room, random }, callback) => {
+        const { error, user } = addUser({ id: socket.id, room: room, random });
         // console.log(user, error);
 
         if (error) {
-            callback({error});
+            callback({ error });
             return socket.disconnect(true);
         }
 
-
-
-        socket.emit('connected', {player: user.name, room: user.room, boardState: getGameState(user.room)});
-        socket.emit('message', {user: 'admin', text: 'Welcome to the game!'});
-        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `User ${user.name} has joined the game!`});
+        socket.emit("connected", {
+            player: user.name,
+            room: user.room,
+            boardState: getGameState(user.room),
+        });
+        socket.emit("message", { user: "admin", text: "Welcome to the game!" });
+        socket.broadcast.to(user.room).emit("message", {
+            user: "admin",
+            text: `User ${user.name} has joined the game!`,
+        });
         socket.join(user.room);
 
-
         callback({});
-
     });
 
-    socket.on('sendMessage', (message, callback) => {
-        console.log('send message');
+    socket.on("sendMessage", (message, callback) => {
+        console.log("send message");
         const user = getUser(socket.id);
         if (user) {
-            io.to(user.room).emit('message', {user: user.name, text: message});
+            io.to(user.room).emit("message", {
+                user: user.name,
+                text: message,
+            });
         }
 
         callback();
-
     });
 
-
-    socket.on('startGame', (initialBoardState, callback) => {
-
+    socket.on("startGame", (initialBoardState, callback) => {
         const user = getUser(socket.id);
         // if(getGameState(user.room)) callback(getGameState(user.room));
         // else
-         startGame(initialBoardState, user.room);
-
+        startGame(initialBoardState, user.room);
     });
 
-
-
-    socket.on('move', (moveData, callback) => {
-     console.log('move');
+    socket.on("move", (moveData, callback) => {
+        console.log("move");
         const user = getUser(socket.id);
         console.log(user);
         if (user) {
             // movePiece(moveData.position, moveData.destination, moveData.whoMoves, user.room);
-            io.to(user.room).emit('move', moveData);
+            io.to(user.room).emit("move", moveData);
         }
 
         callback();
     });
 
-    socket.on('updateBoardState', (boardState, callback)=>{
-     const user = getUser(socket.id);
-     updateBoardState(boardState, user.room);
+    socket.on("updateBoardState", (boardState, callback) => {
+        const user = getUser(socket.id);
+        updateBoardState(boardState, user.room);
     });
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
         let user = removeUser(socket.id);
-        console.log('user left')
-        if (user) io.to(user.room).emit('message', {user: 'admin', text: `User ${user.name} has left the game!`});
-    })
+        console.log("user left");
+        if (user)
+            io.to(user.room).emit("message", {
+                user: "admin",
+                text: `User ${user.name} has left the game!`,
+            });
+    });
 });
+app.use(express.static("public"));
 app.use(router);
 
-
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
-
-
-
-
-
--- router.js
+++ router.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('server is running');
+router.use("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+    // res.send("/public/index.html");
 });
-
 
 module.exports = router;
